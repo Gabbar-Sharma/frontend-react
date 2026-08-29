6de65c340bdee64b179dc6cbe63629a03bc1858e
Gabbar-Sharma <gabbarsharma75663@gmail.com>
2026-08-29 21:20:17 +0530

    adding app-routing, auth, rhf, redux

--- redux/src/main.jsx
+++ redux/src/main.jsx
@@ -1,7 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './app/App.jsx'
 import {store} from './app/store.jsx'
 import {Provider} from 'react-redux'
 
